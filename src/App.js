efe5b49904c1735a4135aacf52ddf5ca7491dae2
drewbitt <andrew@drewbitt.com>
2019-11-12 11:00:57 -0500

    Remove whatanime dependency that has fs requirement. Add callback. Force one file only. Remove reference to logo in manifest

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 import RootRef from "@material-ui/core/RootRef";
 import { Paper } from "@material-ui/core";
@@ -10,7 +10,6 @@ function App() {
       <header className="App-header">
         <h1>Trace.moe</h1>
         <h4>Trace the original anime from a screenshot.</h4>
-
         <PaperDropzone />
       </header>
     </div>
@@ -18,14 +17,21 @@ function App() {
 }
 
 function PaperDropzone() {
-  const { getRootProps, getInputProps } = useDropzone();
+  const onDrop = useCallback(acceptedFiles => {
+    for (let file in acceptedFiles) {
+      // with multiple=false, is array of size 1
+      
+    }
+  });
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
   const { ref, ...rootProps } = getRootProps();
 
   return (
     <RootRef rootRef={ref}>
-      <Paper {...rootProps} elevation="7" className="FileUpload">
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
+      <Paper {...rootProps} elevation={7} className="FileUpload">
+        <input {...getInputProps()} multiple={false} />
+        <p>Drag 'n' drop a file here, or click to select a file</p>
       </Paper>
     </RootRef>
   );
