3ddf2b5356c29e81d3a92038d3c95ef1d3ccd0ac
drewbitt <andrew@drewbitt.com>
2019-11-13 09:30:04 -0500

    Add previews

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,8 +4,8 @@ class Results extends React.Component {
 
   render() {
 
-    return this.props.location.state.results ? this.props.location.state.results.docs.map(item => {
-      return <li>{item.title_english} - EP: {item.episode} - {item.imagepreview}</li>
+    return this.props.location.state.results ? this.props.location.state.results.docs.map((item, index) => {
+      return <li>Guess #{index+1}: {item.title_english} - EP: {item.episode} - <a href={item.imagepreview}>Image Preview</a> - <a href={item.videopreview}>Video Preview</a> - <a href={item.naturalvideopreview}>Nautral Video Preview</a></li>
     }): <h1>No results</h1>;
   }
 }
