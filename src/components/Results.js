86b2572a098a43af928efaa476c5a802e5034e78
drewbitt <andrew@drewbitt.com>
2019-11-13 12:17:01 -0500

    Fix error if going to /results with no props passed

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,17 +6,24 @@ class Results extends React.Component {
     return (
       <div className="Main">
         <header className="Main-header">
-          <h2>Trace.moe results</h2>
-          {this.props.location.state.results.docs.map((item, index) => {
-            return (
-              <li key={index + 1}>
-                Guess #{index + 1}: {item.title_english} - EP: {item.episode} -{" "}
-                <a href={item.imagepreview}>Image Preview</a> -{" "}
-                <a href={item.videopreview}>Video Preview</a> -{" "}
-                <a href={item.naturalvideopreview}>Natural Video Preview</a>
-              </li>
-            );
-          })}
+          {this.props.location.state
+            ? this.props.location.state.results.docs.map((item, index) => {
+                return (
+                  <div id="result">
+                    {index == 0 ? <h2>Trace.moe results</h2> : ""}
+                    <li key={index + 1}>
+                      Guess #{index + 1}: {item.title_english} - EP:{" "}
+                      {item.episode} -{" "}
+                      <a href={item.imagepreview}>Image Preview</a> -{" "}
+                      <a href={item.videopreview}>Video Preview</a> -{" "}
+                      <a href={item.naturalvideopreview}>
+                        Natural Video Preview
+                      </a>
+                    </li>
+                  </div>
+                );
+              })
+            : "No results"}
         </header>
       </div>
     );
